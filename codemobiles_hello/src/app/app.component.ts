a752d0852a0715fc0e608396bba09dfbc61c805f
codemobiles <chaiyasit.t@gmail.com>
2019-10-05 11:24:10 +0700

    1234

--- codemobiles_hello/src/app/app.component.ts
+++ codemobiles_hello/src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'hello';
+  header = "Angular Communication Demo";
   version = 1
   isGirl = false;
+  time:any;
+
+  onClickRun1(){
+    alert("555");
+  }
+
+  ngOnInit(): void {
+   setInterval(()=>{
+    this.time = Date.now();
+   }, 1000)
+  }
 
   isUpgrade(){
     return "yes";
