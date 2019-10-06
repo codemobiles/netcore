df4f5229606399e31cb47066068b94c9683c6418
iBlurBlur <tanakorn.ngam@gmail.com>
2019-10-06 13:38:03 +0700

    ...

--- lekPOS/readme.txt
+++ lekPOS/readme.txt
@@ -3,4 +3,4 @@ Angular2-switcher
 Angular 8 Snippets typeScript, Html, ….. -
 Angular Snippets (Version 8)
 Angular Language Service.
-Angular Schematics.   
+Angular Schematics.
--- lekPOS/src/app/components/stock/stock-home/stock-home.component.ts
+++ lekPOS/src/app/components/stock/stock-home/stock-home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NetworkServiceService } from 'src/app/services/network-service.service';
 import { TestJSON } from 'src/app/models/test.model';
-
+import Swal from 'sweetalert2'
 @Component({
   selector: 'app-stock-home',
   templateUrl: './stock-home.component.html',
@@ -27,6 +27,20 @@ export class StockHomeComponent implements OnInit {
 
   getOutOfStock(): number {
     return 10;
+  }
+
+  deleteProduct(id: number){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You won\'t be able to revert this!',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then(async result => {
+        // todo
+    });
   }
 
 }
